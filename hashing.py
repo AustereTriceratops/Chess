e5a0d461ad97377453e41e70df1064d00499b69a
Levi Walker <gatewaylevi@gmail.com>
2020-07-23 18:20:53 -0500

    transposition tables

---
+++ hashing.py
@@ -0,0 +1,26 @@
+import random as r
+
+
+zobrist_keys = [[r.randint(1, 2**64 - 1) for _ in range(12)] for _ in range(64)]
+
+
+def hash_board(board):  # chess.Boars
+    key = 0
+
+    for i in range(64):
+        if board.color_at(i) is not None:
+            ind = board.piece_type_at(i) + 6 * board.color_at(i) - 1  #abuse of type coercion
+            key ^= zobrist_keys[i][ind]
+            
+    return key
+
+
+def rehash(h_0, board, move):  # pre-move board, chess.Move
+    newhash = h_0
+    from_sq = move.from_square
+    to_sq = move.to_square
+    ind = board.piece_type_at(from_sq) + 6 * board.color_at(from_sq) - 1
+    
+    newhash ^= zobrist_keys[from_sq][ind]
+    newhash ^= zobrist_keys[to_sq][ind]
+    return newhash
--- search.py
+++ search.py
@@ -1,4 +1,5 @@
 from evaluate import evaluate
+from hashing import *
 
 
 class Node():
@@ -24,6 +25,28 @@ class Node():
             child.function_on_tree(func, *args, **kwargs)
 
 
+def create_variations_2(node):  # Node[]
+    variations = []
+    data = node.data.copy()
+    board = data["board"]
+    zobrist_key = data["zobrist_key"]
+
+    for move in list(board.legal_moves):
+        v = board.copy()
+        
+        zobrist_key = rehash(zobrist_key, v, move)
+        data['zobrist_key'] = zobrist_key
+        data['hashval'] = zobrist_key % 10000
+        
+        v.push(move)
+
+        data["board"] = v
+        data["move"] = move
+        variations.append(Node(parent=node, **data))
+
+    return variations
+
+
 def create_variations(node):  # Node[]
     variations = []
     data = node.data.copy()
@@ -40,6 +63,76 @@ def create_variations(node):  # Node[]
     return variations
 
 
+def alpha_beta_search_2(node, depth, scoring_dict, variation_func, alpha, beta, transposition_table={i: None for i in range(10000)}):
+    color = node.data["board"].turn
+    transposition_table[node.data["hashval"]] = 1
+
+    if depth == 0 or node.data['board'].is_game_over():
+        node.data['score'] = evaluate(node.data["board"], scoring_dict)
+        return
+
+    if not node.children:
+        node.create_children(variation_func)
+
+    if color:
+        maxval = -100000000
+        best_move = None
+
+        for child in node.children:
+            hashval = child.data['hashval']
+            
+            if transposition_table[hashval] == 1:
+                continue
+            else:
+                transposition_table[hashval] = 1 #child.data['zobrist_key']
+            
+            alpha_beta_search_2(
+                child, depth - 1, scoring_dict, variation_func, alpha, beta, transposition_table=transposition_table
+            )
+                
+            score = child.data['score']
+            maxval = max(maxval, score)
+
+            if maxval == score:
+                best_move = child.data['move']
+
+            alpha = max(alpha, score)
+            if beta <= alpha:
+                break
+
+        node.data['score'] = maxval
+        return best_move
+
+    else:
+        minval = 100000000
+        best_move = None
+
+        for child in node.children:
+            hashval = child.data['hashval'] 
+            
+            if transposition_table[hashval] == 1:
+                continue
+            else:
+                transposition_table[hashval] = 1 # child.data['zobrist_key']
+                
+            alpha_beta_search_2(
+                child, depth - 1, scoring_dict, variation_func, alpha, beta, transposition_table=transposition_table
+                             )
+            
+            score = child.data['score']
+            minval = min(minval, score)
+
+            if minval == score:
+                best_move = child.data['move']
+
+            beta = min(beta, score)
+            if beta <= alpha:
+                break
+
+        node.data['score'] = minval
+        return best_move
+
+    
 def alpha_beta_search(node, depth, scoring_dict, variation_func, alpha, beta):
     color = node.data["board"].turn
 
